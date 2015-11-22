2c809683a95f9789ac23e3fe67f3db3daec18b14
endroid <info@endroid.nl>
2015-11-22 21:22:32 +0100

    Implement deployment example

---
+++ app/config/deploy.rb
@@ -0,0 +1,35 @@
+set :symfony_console, "bin/console"
+set :log_path, "var/logs"
+set :cache_path, "var/cache"
+
+set :application, "symfony-demo.endroid.nl"
+set :domain, "#{application}"
+set :deploy_to, "/home/endroid/vhosts/#{domain}"
+
+set :repository, "git@github.com:endroid/symfony-demo.git"
+set :scm, :git
+
+ssh_options[:use_agent] = false
+
+set :user, "endroid"
+set :use_sudo, false
+
+role :web, domain
+role :app, domain, :primary => true
+
+set :keep_releases, 2
+
+set :shared_children, [log_path, cache_path, web_path + "/uploads"]
+
+before "deploy:finalize_update" do
+    run "cp #{release_path}/../../app/config/parameters.yml #{release_path}/app/config/parameters.yml"
+    run "cp #{release_path}/../../reset.sh.dist #{release_path}/reset.sh"
+end
+
+after "deploy:finalize_update" do
+  run "setfacl -R -m u:www-data:rwx -m u:endroid:rwx #{latest_release}/#{cache_path} #{latest_release}/#{log_path} #{latest_release}/web/uploads"
+  run "setfacl -dR -m u:www-data:rwx -m u:endroid:rwx #{latest_release}/#{cache_path} #{latest_release}/#{log_path} #{latest_release}/web/uploads"
+  run "cd #{latest_release} && sh reset.sh"
+end
+
+after "deploy:restart", "deploy:cleanup"
